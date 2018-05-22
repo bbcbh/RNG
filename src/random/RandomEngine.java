e59dc624db81093f7da4b0f04eca60b251bf1d79
Bhui <Bhui@KIRBYZ240W10-2.ad.nchecr.unsw.edu.au>
2018-05-22 11:04:18 +1000

    Set RandomEngine as deprecated

--- src/random/RandomEngine.java
+++ src/random/RandomEngine.java
@@ -5,6 +5,7 @@ package random;
  * A wrapper class of <i>RandomEngine</i> class as defined by <a href="http://acs.lbl.gov/software/colt/">Colt</a>
  * numerical library.
  *
+ * @deprecated  Use RandomGenerator instead
  * @author Ben Hui
  */
 public abstract class RandomEngine extends cern.jet.random.engine.RandomEngine  {
